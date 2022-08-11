2319cb44f31d06d352d660535451466628a3690c
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-08-12 04:14:14 +0500

    feat: towel sort

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 module.exports = function towelSort (matrix) {
+    if (!matrix) return [];
     return matrix.reduce((a, b) => {
         return a.concat(Array.isArray(b) ? towelSort(b) : b);}, []);
 }
